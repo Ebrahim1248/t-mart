20847e9a9ff697f25b0b568fa86614795dfe7cf8
Ebrahim1248 <mdjaver390@gmail.com>
2025-12-01 00:46:14 +0600

    src/component/Home/Banner.jsx

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <>
         <section className=" pb-12">
-            <div className="relative container bg-[url('/Background.png')] bg-no-repeat bg-cover bg-center py-[310px]">
+            <div className="relative container bg-[url('C:\Users\MD MUSA\OneDrive\Creative Cloud Files\Desktop\t-mart\public\Background.png')] bg-no-repeat bg-cover bg-center py-[310px]">
                 <div className="absolute left-[50%] top-[40%] ">
                   <h1 className='text-3xl font-normal text-primary max-w-[432px]  m-auto pb-6'>Awesome Product Collection In 2022</h1>
                   <Link to="/shop" className="inline-block rounded-md ">Shop Now</Link>
