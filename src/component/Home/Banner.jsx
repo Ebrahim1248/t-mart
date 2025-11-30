c3c53902a6c38008a9b1412837f9d91e03ddea65
Ebrahim1248 <mdjaver390@gmail.com>
2025-12-01 01:02:30 +0600

    src

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -5,11 +5,14 @@ const Banner = () => {
   return (
     <>
         <section className=" pb-12">
-            <div className="relative container bg-no-repeat bg-cover bg-center py-[310px]" style={{backgroundImage: "url('/Background.png')"}}>
-                <div className="absolute left-[50%] top-[40%] ">
-                  <h1 className='text-3xl font-normal text-primary max-w-[432px]  m-auto pb-6'>Awesome Product Collection In 2022</h1>
-                  <Link to="/shop" className="inline-block rounded-md ">Shop Now</Link>
-                </div>
+            <div className=" container flex justify-around  py-[310px]" >
+              <div className="">
+                <img src="/Home-banner.png" alt="Home-banner" />
+              </div>
+              <div className="a ">
+                <h1 className='text-3xl font-normal text-primary max-w-[432px]  m-auto pb-6'>Awesome Product Collection In 2022</h1>
+                <Link to="/shop" className="inline-block rounded-md ">Shop Now</Link>
+              </div>
 
             </div>
         </section>
