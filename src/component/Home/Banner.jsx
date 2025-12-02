14ac6d2537cef18da67ac6440cbea5d6d02dec70
Md.Ebrahim Kholil <mdjaver390@gmail.com>
2025-12-02 08:08:46 -0600

    Update image width in Banner component

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
         <section className="bg-background pb-12">
             <div className=" container md:flex items-center justify-around  py-10" >
               
-              <img src="/Home-banner.png" alt="Home-banner" className='max-w-[432px]'/>
+              <img src="/Home-banner.png" alt="Home-banner" className='w-[150px] md:w[250px]  lg:max-w-[432px]'/>
               
               <div className=" ">
                 <h1 className='text-3xl font-normal text-primary max-w-[432px]  m-auto pb-6'>Awesome Product Collection In 2022</h1>
